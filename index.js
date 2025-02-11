79ece7c8320bc7962ba789be6b30142ffff0f79c
Giselle De Leon <g.deleon.126668.tc@umindanao.edu.ph>
2025-02-11 10:15:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ import bodyParser from "body-parser";
 import pool from "./db.js"; // Import the database connection
 import "dotenv/config";
 
-const app = express();
-const port = process.env.PORT || 3000;
-
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
